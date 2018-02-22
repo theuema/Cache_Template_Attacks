2146ea390a5533549b3c09210b8a252de7f21f38
Mario Theuermann <mario.theuermann@student.tugraz.at>
2018-02-22 09:36:11 +0100

    added log output to file for automated cache simulation comparison;

--- calibration/calibration.c
+++ calibration/calibration.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sched.h>
+#include <assert.h>
 #include "../cacheutils.h"
 
 size_t array[5*1024];
@@ -50,7 +51,11 @@ int main(int argc, char** argv)
   size_t miss_min_i = 0;
   for (int i = 0; i < 80; ++i)
   {
-    printf("%3d: %10zu %10zu\n",i*5,hit_histogram[i],miss_histogram[i]);
+    FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+    assert(fc != NULL);
+    fprintf(fc, "%3d: %10zu %10zu\n",i*5,hit_histogram[i],miss_histogram[i]);
+    fclose(fc);
+    //printf("%3d: %10zu %10zu\n",i*5,hit_histogram[i],miss_histogram[i]);
     if (hit_max < hit_histogram[i])
     {
       hit_max = hit_histogram[i];
@@ -59,14 +64,38 @@ int main(int argc, char** argv)
     if (miss_histogram[i] > 3 && miss_min_i == 0)
       miss_min_i = i;
   }
-  if (miss_min_i > hit_max_i+4)
+  if (miss_min_i > hit_max_i+4){
+    FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+    assert(fc != NULL);
+    fprintf(fc, "Flush+Reload possible!\n");
+    fclose(fc);
     printf("Flush+Reload possible!\n");
-  else if (miss_min_i > hit_max_i+2)
+  }
+
+  else if (miss_min_i > hit_max_i+2){
+    FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+    assert(fc != NULL);
+    fprintf(fc, "Flush+Reload probably possible!\n");
+    fclose(fc);
     printf("Flush+Reload probably possible!\n");
-  else if (miss_min_i < hit_max_i+2)
+  }
+
+  else if (miss_min_i < hit_max_i+2){
+    FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+    assert(fc != NULL);
+    fprintf(fc, "Flush+Reload maybe not possible!\n");
+    fclose(fc);
     printf("Flush+Reload maybe not possible!\n");
-  else
+  }
+
+  else{
+    FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+    assert(fc != NULL);
+    fprintf(fc, "Flush+Reload not possible!\n");
+    fclose(fc);
     printf("Flush+Reload not possible!\n");
+  }
+
   size_t min = -1UL;
   size_t min_i = 0;
   for (int i = hit_max_i; i < miss_min_i; ++i)
@@ -77,6 +106,12 @@ int main(int argc, char** argv)
       min_i = i;
     }
   }
+
+  FILE *fc = fopen("logs/calibration_hist_current.txt", "a");
+  assert(fc != NULL);
+  fprintf(fc, "The lower the threshold, the lower the number of false positives.\n");
+  fprintf(fc, "Suggested cache hit/miss threshold: %zu\n",min_i * 5);
+  fclose(fc);
   printf("The lower the threshold, the lower the number of false positives.\n");
   printf("Suggested cache hit/miss threshold: %zu\n",min_i * 5);
   return min_i * 5;
